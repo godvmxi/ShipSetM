62466556d2c09888f89990ebce58fd8752832e87
godvmxi <godvmxi@126.com>
2015-10-15 23:54:11 +0800

    fix basic database

--- app/src/main/java/com/juntuanyi/www/shipsetm/MainActivity.java
+++ app/src/main/java/com/juntuanyi/www/shipsetm/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         linearLayoutTankList = (LinearLayout) findViewById(R.id.linearLayoutTankList);
         textViewTopInfo = (TextView) findViewById(R.id.textViewTopInfo);
+        this.initDemoDatabase((Context)this);
 
         tankViewEvent = new TankViewEvent() {
             @Override
@@ -61,57 +62,9 @@ public class MainActivity extends Activity {
 
         this.updateTankViewItem();
         Log.d(logTag,"total object number -> " +  tankViewList.size());
-//        tankView1 = (TankView) findViewById(R.id.tankView1);
-//
-//        tankView1.setInfo(new TankInfo( 1));
-//        tankView1.setTankViewEvent(tankViewEvent);
-//        tankView2 = (TankView) findViewById(R.id.tankView2);
-//        tankView2.setInfo(new TankInfo( 2));
-//        tankView2.setTankViewEvent(tankViewEvent);
 
 
-//        db.execSql("Create table help");
-
-
-//        File dbFile =  context.getDatabasePath("demo.db");
-//        Log.d(logTag,"---> "+dbFile.getAbsolutePath());
-
-
-        String filesDir =  context.getFilesDir().getAbsolutePath() ;
-        Log.d(logTag,"file dir-> "+filesDir.toString());
-        String baseDataDir = filesDir.replaceAll("files","") ;
-        String databasesDir = baseDataDir + "/databases" ;
-        String demoDbFileName = databasesDir + "/demo.db3" ;
-        Log.d(logTag,"database dir-> "+demoDbFileName);
-        Log.d(logTag,"files dir-> "+filesDir);
-        Log.d(logTag,"base dir-> "+baseDataDir);
-
-        if ( !(new File(databasesDir)).exists()  ) {
-            (new File(databasesDir)).mkdir() ;
-        }
-        else {
-            Log.d(logTag,"no need to create databases dir");
-        }
-        if(!( new File(demoDbFileName) ).exists() ){ //no demo db file ,copy
-            Resources resources  = this.getResources();
-            InputStream dbRawStream = resources.openRawResource(R.raw.demo);
-            Log.d(logTag,"---> "+dbRawStream.toString());
-            try {
-                FileOutputStream fos = new FileOutputStream(demoDbFileName);
-                byte[] buffer = new byte[8192];
-                int count = 0;
-                while ((count = dbRawStream.read(buffer)) > 0) {
-                    fos.write(buffer, 0, count);
-                }
-//关闭资源
-                fos.close();
-                dbRawStream.close();
-            }
-            catch (Exception e) {
-                Log.e(logTag, "copy db from res to cache error");
-            }
-        }
-        this.dbHandler = new SqlUtils(context ,"demo.db3");
+        this.dbHandler = new SqlUtils(context ,"ships.db3");
         textViewTopInfo.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -157,6 +110,42 @@ public class MainActivity extends Activity {
             tankViewList.add(view) ;
 
             linearLayoutTankList.addView((View)view);
+        }
+    }
+    public void initDemoDatabase(Context context){
+        String filesDir =  context.getFilesDir().getAbsolutePath() ;
+        Log.d(logTag,"file dir-> "+filesDir.toString());
+        String baseDataDir = filesDir.replaceAll("files","") ;
+        String databasesDir = baseDataDir + "/databases" ;
+        String demoDbFileName = databasesDir + "/ships.db3" ;
+        Log.d(logTag,"database dir-> "+demoDbFileName);
+        Log.d(logTag,"files dir-> "+filesDir);
+        Log.d(logTag,"base dir-> "+baseDataDir);
+
+        if ( !(new File(databasesDir)).exists()  ) {
+            (new File(databasesDir)).mkdir() ;
+        }
+        else {
+            Log.d(logTag,"no need to create databases dir");
+        }
+        if(!( new File(demoDbFileName) ).exists() ){ //no demo db file ,copy
+            Resources resources  = this.getResources();
+            InputStream dbRawStream = resources.openRawResource(R.raw.demo);
+            Log.d(logTag,"---> "+dbRawStream.toString());
+            try {
+                FileOutputStream fos = new FileOutputStream(demoDbFileName);
+                byte[] buffer = new byte[8192];
+                int count = 0;
+                while ((count = dbRawStream.read(buffer)) > 0) {
+                    fos.write(buffer, 0, count);
+                }
+//关闭资源
+                fos.close();
+                dbRawStream.close();
+            }
+            catch (Exception e) {
+                Log.e(logTag, "copy db from res to cache error");
+            }
         }
     }
 
